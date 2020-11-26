af8eb44de2d12ae5ee6150bd46fb71d2cff9ea85
matteogara <273z919o>
2020-11-26 22:20:25 +0100

    Check

---
+++ Assets/02_SCRIPT/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+
+    int time;
+
+
+
+    void StartTimer() {
+        StartCoroutine(UpdateTimer());
+    }
+
+
+    void PauseTimer() {
+        StopAllCoroutines();
+    }
+
+
+    void StopTimer() {
+        StopAllCoroutines();
+        time = 0;
+        textMesh.text = "0:00";
+    }
+
+
+    IEnumerator UpdateTimer() {
+        yield return new WaitForSeconds(1);
+
+        time++;
+        string mins = Mathf.Floor(time / 60).ToString();
+        string secs = (time % 60).ToString("00");
+        textMesh.text = mins + ":" + secs;
+
+        StartCoroutine(UpdateTimer());
+    }
+}
