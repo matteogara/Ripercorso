07477d8a17032a7ee9c3d27df0a370aa3c4342c1
matteogara <273z919o>
2020-11-26 03:40:11 +0100

    SpeechRecognition Yeee

---
+++ Assets/02_SCRIPT/UpdateText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateText : MonoBehaviour
+{
+    TextMeshPro textMesh;
+
+
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshPro>();
+    }
+
+
+    public void OnInterimResults(string youSaid)
+    {
+        textMesh.text = youSaid;
+    }
+
+
+    public void OnFinalResult(string youSaid)
+    {
+        textMesh.text = youSaid;
+    }
+}
